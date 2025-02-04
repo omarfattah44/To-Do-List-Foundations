fa4d26767ac920fae9a81bad7f43e4930b7b4e71
omarfattah44 <omarmfattah44@gmail.com>
2025-02-04 14:45:22 -0800

    added DOM elements and event listeners for button

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('My code is running!');
+// These will be the element on the page that will be interacted with as variables
+const taskInput = document.getElementById('todo-input');
+const addButton = document.getElementById('add-button');
+const Tasks = document.getElementById('Tasks');
+
+// This will add the event listeners: 
+addButton.addEventListener('click', addtask); // this one is used for the clicker
+
+taskInput.addEventListener('keypress', function (event) {// this one is used for the enter key
+  if (event.key === 'Enter') {
+    addtask();
+  }
+}); 
